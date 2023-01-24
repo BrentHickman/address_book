46efd40466584c322e10808e3fc310f01e7c87e7
brenthickman <brenthickman@ymail.com>
2023-01-24 09:29:35 -0800

    initial commit for tuesday
    Co-authored-by: Hernan Verar <hernanverar@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 let myAddressBook = new AddressBook();
 
-function Contact(addressName, addressAddress, addressPhone){
+function Contact(addressName, addressAddress, addressPhone, addressEmail){
     this.addressName = addressName;
     this.addressAddress = addressAddress;
-    this.addressPhone = addressPhone
+    this.addressPhone = addressPhone;
+    this.addressEmail = addressEmail;
 }
 
 function AddressBook(){
@@ -52,29 +53,45 @@ function addNewLine(passedInId){
     newLink.innerText =  "Name: " + myAddressBook.findContact(passedInId).addressName;
     newLine.append(newLink);
     document.getElementById("ulSpace").append(newLine);
+    
     let newUl = document.createElement('ul');
     newUl.setAttribute('id', "ul" + passedInId + "1");
     newLine.append(newUl);
+    
     let newUl2 = document.createElement('ul');
     newUl2.setAttribute('id', "ul" + passedInId + "2");
     newLine.append(newUl2);
+
+    let newUl3 = document.createElement('ul');
+    newUl3.setAttribute('id', "ul" + passedInId + "3");
+    newLine.append(newUl3);
+    
     let hiddenTextAddress = document.createElement('li');
     hiddenTextAddress = "Address: " + myAddressBook.findContact(passedInId).addressAddress;
     document.getElementById("ul" + passedInId + "1").append(hiddenTextAddress);
+    
     let hiddenTextNumber = document.createElement('li');
     hiddenTextNumber = "Number: " + myAddressBook.findContact(passedInId).addressPhone;
     document.getElementById("ul" + passedInId + "2").append(hiddenTextNumber);
+
+    let hiddenTextEmail = document.createElement('li');
+    hiddenTextEmail = "Email: " + myAddressBook.findContact(passedInId).addressEmail;
+    document.getElementById("ul" + passedInId + "3").append(hiddenTextEmail);
+
     document.getElementById("ul" + passedInId + "1").setAttribute('class', 'hidden');
     document.getElementById("ul" + passedInId + "2").setAttribute('class', 'hidden');
+    document.getElementById("ul" + passedInId + "3").setAttribute('class', 'hidden');
 }
 
 function showMore(elementToToggle){
     if (document.getElementById("ul" + elementToToggle + "1").getAttribute("class") === "hidden"){
         document.getElementById("ul" + elementToToggle + "1").setAttribute("class", "notHidden");
         document.getElementById("ul" + elementToToggle + "2").setAttribute("class", "notHidden"); 
+        document.getElementById("ul" + elementToToggle + "3").setAttribute("class", "notHidden"); 
     } else {
         document.getElementById("ul" + elementToToggle + "1").setAttribute("class", "hidden");
         document.getElementById("ul" + elementToToggle + "2").setAttribute("class", "hidden"); 
+        document.getElementById("ul" + elementToToggle + "3").setAttribute("class", "hidden"); 
     }
     //find the element we want
     //check if it is hidden
@@ -95,19 +112,10 @@ function onSubmit(event){
     let tempContact = new Contact(
         document.getElementById("placeNameId").value, 
         document.getElementById("placeAddressId").value, 
+        document.getElementById("placePhoneId").value,
         document.getElementById("placePhoneId").value);
 
     myAddressBook.addContact(tempContact);
     addNewLine(myAddressBook.currentId);
 }
 
-
-
-
-// let testContact = new Contact("ada", "lovelace", "808-555-1111");
-// let testContact2 = new Contact("john", "smith", "123-321-1234");
-// let newAddressBook = new AddressBook();
-// newAddressBook.addContact(testContact);
-// newAddressBook.addContact(testContact2);
-
-// console.log(newAddressBook.contacts["ada"].phoneNumber);
